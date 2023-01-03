36e030738e40a3f5967f84888ab4a4b62865a231
Uzair-Sohail <uzairali051@gmail.com>
2023-01-03 22:23:28 +0500

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+      // Create a React App which greets you based upon a specfic time of day.
+      //Show a single h1 that says "Good morning" if between midnight and 12PM.
+      //or "Good Afternoon" if between 12PM and 6PM.
+      //or "Good evening" if between 6PM and midnight.
+      //Apply the "heading" style in the styles.css
+      //Dynamically change the color of the h1 using inline css styles.
+      //Morning = red, Afternoon = green, Night = blue.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const date = new Date();
+let greeting;
+let color;
+const name = 'Uzair';
+const hours = date.getHours();
+if (hours>=0 && hours<12) {
+  greeting = "Good morning";
+  color = "red";
+} else if (hours>=12 && hours<18) {
+  greeting = "Good afternoon";
+  color = "green";
+} else {
+  greeting = "Good evening";
+    color='blue';
+}  
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <React.StrictMode>
+    {
+      <h1 className="heading" style= {{color:color}}> {greeting} {name}</h1>
+    }
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
