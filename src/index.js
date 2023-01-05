285af3be2db989ff718c508172bc40620a1a6696
Uzair-Sohail <uzairali051@gmail.com>
2023-01-05 23:45:14 +0500

    Master

--- src/index.js
+++ src/index.js
@@ -3,38 +3,41 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-      // Create a React App which greets you based upon a specfic time of day.
-      //Show a single h1 that says "Good morning" if between midnight and 12PM.
-      //or "Good Afternoon" if between 12PM and 6PM.
-      //or "Good evening" if between 6PM and midnight.
-      //Apply the "heading" style in the styles.css
-      //Dynamically change the color of the h1 using inline css styles.
-      //Morning = red, Afternoon = green, Night = blue.
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const date = new Date();
-let greeting;
-let color;
-const name = 'Uzair';
-const hours = date.getHours();
-if (hours>=0 && hours<12) {
-  greeting = "Good morning";
-  color = "red";
-} else if (hours>=12 && hours<18) {
-  greeting = "Good afternoon";
-  color = "green";
-} else {
-  greeting = "Good evening";
-    color='blue';
-}  
+import Card from "./Card";
+import contacts from "./Contact";
 
-root.render( 
+//Creating Contact App using Props and components
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(
   <React.StrictMode>
     {
-      <h1 className="heading" style= {{color:color}}> {greeting} {name}</h1>
+      <div>
+        <h1 className="heading">My Contacts</h1>
+        <Card
+          name={contacts[0].name}
+          img={contacts[0].img}
+          tel={contacts[0].phone}
+          email={contacts[0].email}
+        />
+        <Card
+          name={contacts[1].name}
+          img={contacts[1].img}
+          tel={contacts[1].phone}
+          email={contacts[1].email}
+        />
+        <Card
+          name={contacts[2].name}
+          img={contacts[2].img}
+          tel={contacts[2].phone}
+          email={contacts[2].email}
+        />
+      </div>
     }
     <App />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
