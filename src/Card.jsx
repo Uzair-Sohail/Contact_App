e707a123057252225d43477a29ec00401b824332
Uzair-Sohail <uzairali051@gmail.com>
2023-01-14 13:09:16 +0500

    Master

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-function Card(Props) {
+function Card(name,img,tel,email) {
   return (
     <div className="card">
       <div className="top">
-        <h2 className="name">{Props.name}</h2>
-        <img className="circle-img" src={Props.img} alt="" />
+        <h2 className="name">{name}</h2>
+        <img className="circle-img" src={img} alt="" />
         </div>
         <div className="bottom">
-        <p className="info">{Props.tel}</p>
-        <p className="info">{Props.email}</p>
+        <p className="info">{tel}</p>
+        <p className="info">{email}</p>
       </div>
     </div>
   );
